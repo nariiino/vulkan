0288c63fcbf35a417d6a6427e645217c6022a22c
nariiino <sophiamaiorino@gmail.com>
2024-05-02 10:22:12 -0700

    nmnmn

--- endsketch.js
+++ endsketch.js
@@ -1,38 +1,39 @@
 
+
+
 let one = { //Green Circle
     x: 0,
     y: 0,
-    d: 20
+    d: 30
   };
   
   let two = { //Magenta Circle
     x: 0,
     y: 0,
-    d: 20
+    d: 30
   };
   
   let three = { //Orange Circle
     x: 0,
     y: 0,
-    d: 20
+    d: 30
   };
   
   
   
   function preload() {
-    r = loadImage('r4.gif');
-    r2 = createImg('r4.gif', 'explosion')
+    interactable = loadImage('r4.gif');
+    interactable2 = loadImage('r4.gif');
+    interactable1 = loadImage('r4.gif');
     bg = loadImage('bg.gif')
   }
   
   
-  
-
-  function windowResized(){
-    resizeCanvas(windowWidth,windowHeight);
-  }
-  function setup() {
-    createCanvas(windowWidth,windowHeight);
+    function windowResized(){
+      resizeCanvas(windowWidth,windowHeight);
+    }
+    function setup() {
+      createCanvas(windowWidth,windowHeight);
     noStroke();
     one.x = random(0, 500);
     one.y = random(0, 500);
@@ -45,6 +46,11 @@ let one = { //Green Circle
   function draw() {
     
     background(bg);
+    
+   image(interactable, three.x, three.y,);
+   image(interactable2, two.x, two.y);
+   image(interactable1, one.x, one.y);
+
   
     fill('rgb(220,247,220)');
     ellipse(one.x, one.y, one.d, one.d);
@@ -59,6 +65,7 @@ let one = { //Green Circle
     if (one.x > width + 100) {
       one.x = random(0, 500); 
       one.y = 0; 
+      one.d = 20
     }
     
      if (two.x > width + 100) {
@@ -70,12 +77,12 @@ let one = { //Green Circle
      if (three.x > width + 100) {
       three.x = random(0, 500); 
       three.y = 0; 
-       three.d = 30
+       
   
     }
     
     
-     one.x += 1; 
+    one.x += 1; 
     one.y += 1; 
     
     two.x += 1; 
@@ -87,8 +94,20 @@ let one = { //Green Circle
     
     if (mouseIsPressed) {
       click();
-      r2.position(three.x, three.y);
+     
+      press();
+      
+      hit();
     } 
+  
+  }
+  
+  
+  function mousePressed() {
+   
+    //interactable.setFrame(0);
+    //interactable.play();
+
   
   }
   
@@ -96,11 +115,35 @@ let one = { //Green Circle
   
   function click() {
     let button = dist(mouseX, mouseY, three.x, three.y);
+    //let button = dist(mouseX, mouseY, two.x, two.y);
     if (button <= 50) {
       three.d = 0 
-      r2.position(three.x, three.y);
+      interactable.setFrame(0);
+    interactable.play();
       console.log("click!");
     }
+}
+
+    function press() {
+        let button = dist(mouseX, mouseY, two.x, two.y);
+        //let button = dist(mouseX, mouseY, two.x, two.y);
+        if (button <= 50) {
+          two.d = 0 
+          interactable2.setFrame(0);
+          interactable2.play();
+          console.log("press!");
+        }
+    }
+
+        function hit() {
+            let button = dist(mouseX, mouseY, one.x, one.y);
+            if (button <= 50) {
+              one.d = 0 
+              interactable1.setFrame(0);
+              interactable1.play();
+              console.log("hit!");
+            }
+}
+
     
-    
-  }
+  
